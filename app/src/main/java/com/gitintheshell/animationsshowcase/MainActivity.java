00f14d8bc70db939bf8529d0d4329689e9c9f455
Semyon <samvimes@yandex.ru>
2017-06-04 00:44:45 +0300

    Initial commit

---
+++ app/src/main/java/com/gitintheshell/animationsshowcase/MainActivity.java
@@ -0,0 +1,35 @@
+package com.gitintheshell.animationsshowcase;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.basic).setOnClickListener(new TransitionButtonClickListener(BasicAnimActivity.class));
+        findViewById(R.id.image_transform).setOnClickListener(new TransitionButtonClickListener(ImageTransformActivity.class));
+        findViewById(R.id.interpolator).setOnClickListener(new TransitionButtonClickListener(InterpolatorActivity.class));
+    }
+
+    private class TransitionButtonClickListener implements View.OnClickListener {
+
+        private Class<? extends Activity> clazz;
+
+        public TransitionButtonClickListener(final Class<? extends Activity> clazz) {
+            this.clazz = clazz;
+        }
+
+        @Override
+        public void onClick(final View v) {
+            final Activity activity = MainActivity.this;
+            Intent intent = new Intent(activity, clazz);
+            startActivity(intent);
+        }
+    }
+}
